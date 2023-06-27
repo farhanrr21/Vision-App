a914fc4a27cc14202f9bef570e699e673086cb53
Ryan <farhanrafli85@gmail.com>
2023-06-28 04:19:57 +0700

    Vision App UI

---
+++ app/src/main/java/com/example/blindapp/Fitur/Kalender.java
@@ -0,0 +1,16 @@
+package com.example.blindapp.Fitur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.blindapp.R;
+
+public class Kalender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kalender);
+    }
+}
